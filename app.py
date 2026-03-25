8d8a97dee017f370b1345380a80ef8e4b41046aa
Vijay <vijayrampeesa59@gmail.com>
2026-03-25 15:26:39 +0530

    Add /submittodoitem MongoDB POST

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import app, request
+from pymongo import MongoClient
+
+# MongoDB Atlas connection
+MONGO_URI = "mongodb+srv://dummy:1234@tram.uzwi1fl.mongodb.net/?appName=tram"
+mongo_client = MongoClient(MONGO_URI)
+db = mongo_client['Flask']
+mongo = db['Data']
+
+@app.route('/submittodoitem', methods=['POST'])
+def submittodoitem():
+    item_name = request.form['itemName']
+    item_desc = request.form['itemDescription']
+    todo_item = {"itemName": item_name, "itemDescription": item_desc}
+    mongo.db.todos.insert_one(todo_item)
+    return "Item added to DB!", 201
